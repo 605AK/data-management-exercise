c0c750f1b3eedb7a4f2146adb42acf2c67184213
605AK <135556437+605AK@users.noreply.github.com>
2024-07-04 15:06:01 +0900

    HoT: initial revision

---
+++ HeadsOrTails.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+  int i;
+  int head = 0, tail = 0;
+  int random;
+
+  srand((unsigned)time(NULL));
+  printf("Tossing a coin...\n");
+  for(i = 1; i < 4; i++){
+    printf("Round %d: ",i);
+    random = rand() % 5 + 1;
+    if(random > 0 && random < 4){
+      printf("Heads\n");
+      head++;
+    } else {
+      printf("Tails\n");
+      tail++;
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",head,tail);
+  return 0;
+}
